9d7442a9367f1c16ca19fa953c61e2a2a6d0d6f0
ArthurNahapetian <arturnahapetyan05@gmail.com>
2023-12-22 20:08:00 +0400

    save progress

---
+++ src/components/web-page/section.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import './style.css'
+import firstImage from '../../assets/images/first.png'
+import secondImage from '../../assets/images/second.png'
+import threeImage from '../../assets/images/three.png'
+import fourImage from '../../assets/images/four.png'
+
+class Seaction extends React.Component {
+    render() {
+        return <seaction>
+box
+
+            <img src={firstImage} alt="" />
+            <img src={secondImage} alt="" />
+            <img src={threeImage} alt="" />
+            <img src={fourImage} alt="" />
+
+        </seaction>
+    }
+}
+export default Seaction
