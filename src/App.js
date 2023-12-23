6d982d695ce5f6be639535030d15f88dd2ef1a12
ArthurNahapetian <arturnahapetyan05@gmail.com>
2023-12-23 04:11:45 +0400

    end

--- src/App.js
+++ src/App.js
@@ -2,13 +2,34 @@ import React from "react";
 import Header from "./components/web-page/header"
 import Seaction from "./components/web-page/section"
 
+
+import firstImage from './assets/images/first.png'
+import secondImage from './assets/images/second.png'
+import threeImage from './assets/images/three.png'
+import fourImage from './assets/images/four.png'
+import car1 from './assets/images/car1.jpg'
+import car2 from './assets/images/car2.jpg'
+import car3 from './assets/images/car3.jpg'
+import car4 from './assets/images/car4.jpg'
+
+
 class App extends React.Component {
   render() {
-    return <div>
+    return <div className="flex">
 
-      <Header />
-      <Seaction />
+      <div className="first-section">
+        <div className="class1">  <Seaction background={"color-red"} miniImg={firstImage} text={'UNLIMITED COLOR VARIANTS'} />
+          <Seaction background={"color-blue"} miniImg={secondImage} text={'MANY REUSABLE ELEMENTS'} /></div>
+        <div className="class1">    <Seaction background={"color-yellow"} miniImg={threeImage} text={'RESPONSIVE LAYOUTE'} />
+          <Seaction background={"color-green"} miniImg={fourImage} text={'CLEAN MODERN CODE'} /></div>
+      </div>
 
+      {/* <div className="car1-seaction">
+        <Seaction img1={car1} />
+        <Seaction img1={car2} />
+        <Seaction img1={car3} />
+        <Seaction img1={car4} />
+      </div> */}
     </div>
   }
 }
--- src/components/web-page/section.jsx
+++ src/components/web-page/section.jsx
@@ -1,21 +1,43 @@
 import React from "react";
 import './style.css'
-import firstImage from '../../assets/images/first.png'
-import secondImage from '../../assets/images/second.png'
-import threeImage from '../../assets/images/three.png'
-import fourImage from '../../assets/images/four.png'
+// import firstImage from '../../assets/images/first.png'
+// import secondImage from '../../assets/images/second.png'
+// import threeImage from '../../assets/images/three.png'
+// import fourImage from '../../assets/images/four.png'
 
 class Seaction extends React.Component {
-    render() {
-        return <seaction>
-box
 
-            <img src={firstImage} alt="" />
+    render() {
+        return <div>
+
+            <div className="box-seaction">  <div className={`box ${this.props.background}`}>
+                <img className={'mini-img'} src={this.props.miniImg} alt='' />
+                <p>{this.props.text}</p>
+
+            </div></div>
+
+
+{/* 
+
+            <div className="car-seaction">
+                <img className="big-img" src={this.props.img1} alt="" />
+                <img className="big-img" src={this.props.img1} alt="" />
+                <img className="big-img" src={this.props.img1} alt="" />
+                <img className="big-img" src={this.props.img1} alt="" />
+
+            </div>
+ */}
+
+
+            {/* 
+            <img src= alt="" />
             <img src={secondImage} alt="" />
             <img src={threeImage} alt="" />
-            <img src={fourImage} alt="" />
+            <img src={fourImage} alt="" /> */}
 
-        </seaction>
+
+        </div >
     }
 }
-export default Seaction
+
+export default Seaction 
